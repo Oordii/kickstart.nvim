fb50b372b94fb73b97d12c13777e863318e99d28
Oordii <kvandrushchenko@gmail.com>
2025-07-19 13:37:06 +0200

    add toggleterm shortcut

--- lua/kickstart/plugins/toggleterm.lua
+++ lua/kickstart/plugins/toggleterm.lua
@@ -2,4 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   config = true,
+  keys = {
+    { '<leader>tt', ':ToggleTerm size=40 direction=horizontal name=desktop <CR>', desc = '[t]oggle [t]erminal', silent = true },
+  },
 }
